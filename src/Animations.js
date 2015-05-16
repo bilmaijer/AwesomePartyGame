ed840be8e33f6065706f54ec8ca30627df92010a
underspikey <janek.timmas@gmail.com>
2015-05-16 22:02:09 +0300

    Little change

---
+++ src/Animations.js
@@ -0,0 +1,6 @@
+/**
+ * Created by Spikey on 16.05.2015.
+ */
+function openCards(a, b){
+
+}
--- src/index.js
+++ src/index.js
@@ -4,26 +4,9 @@
 var GameBoardLayer = cc.Layer.extend({
     sprite: null,
     ctor: function () {
-        //////////////////////////////
-        // 1. super init first
         this._super();
-        /////////////////////////////
-        // 3. add your codes below...
-        // add a label shows "Hello World"
-        // create and initialize a label
-        for (i = 1; i < 6; i++) {
-            for (j = 1; j < 6; j++) {
-                var temp = new ccui.Button();
-                temp.loadTextures("res/CloseNormal.png", "res/CloseSelected.png", " ");
-                temp.x = cc.winSize.width / 5 * i - cc.winSize.width / 10;
-                temp.y = cc.winSize.height / 6 * j + cc.winSize.height / 12;
-                this.addChild(temp, 201);
-            }
-        }
-        var hero = new cc.Sprite("res/favicon.ico");
-        hero.x = cc.winSize.width / 2;
-        hero.y = cc.winSize.height / 12;
-        this.addChild(hero, 250);
+        var a = {};
+        createTable();
         return true;
     }
 });
