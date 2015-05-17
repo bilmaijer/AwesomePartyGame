a9a2074bbecd91dba0ab7be06856c5c140ff7bc3
Karl <karl.valliste@gmail.com>
2015-05-17 10:31:51 +0300

    Pildid lisatud kategooriate kaupa

--- src/methods.js
+++ src/methods.js
@@ -5,18 +5,20 @@ function createTable(s) {
 
     var i;
     var j;
-    var array;
     var kasutatud = [];
-    var uus;
-    var test;
+    var map = [];
 
     //buttonite tegemise asi randomiga
 
     var content = [];
-    for (i = 0; i < 25; i++) {
-        content.push(i)
+    for (i = 0; i < 5; i++) {
+        content.push("res/drinking");
+        content.push("res/sporty");
+        content.push("res/brainy");
+        content.push("res/tutvumis");
+        content.push("res/naugty");
     }
-    var map = [];
+
     var hero = new cc.Sprite("res/favicon.ico");
     hero.x = cc.winSize.width / 2;
     hero.y = cc.winSize.height / 12;
@@ -52,32 +54,31 @@ function createTable(s) {
             var temp = new ccui.Button();
             temp.x = cc.winSize.width / 6 * i;
             temp.y = cc.winSize.height / 6 * j + cc.winSize.height / 12;
-            x = i - 1;
-            y = j - 1;
             temp.loadTextures("res/active.png", "res/a4.png", " ", empty);
-            refresh(x, y, temp);
             s.addChild(temp, 201);
             temp.name = (i - 1).toString() + " " + (j - 1).toString();
-            //console.log(temp.name);
             var randInt = Math.floor(Math.random() * (content.length));
             map[temp.name] = [temp, content[randInt]];
+            refresh(x, y, temp.name);
         }
     }
-    function refresh(x, y, temp) {
+
+    function refresh(x, y, tempName) {
         if (kasutatud.indexOf(x.toString() + " " + y.toString()) == -1) {
             if (asukoht[0] - x == 0 || asukoht[0] - x == 1 || asukoht[0] - x == -1) {
                 if (asukoht[1] - y == 0 || asukoht[1] - y == 1 || asukoht[1] - y == -1) {
-                    temp.loadTextures("res/a5.png", "res/a4.png", " ");
+                    console.log(map[tempName]);
+                    temp.loadTextures(map[tempName][1]+".png",map[tempName][1]+"1.png"," ");
+                    temp.setScale(0.1, 0.1);
+                    //temp.loadTextures("res/active.png", "res/active.png", " ");
                     temp.addTouchEventListener(onMouseUp, this);
                 }
                 else {
                     temp.loadTextures("res/inactive.png", "res/inactive.png", " ");
-
                 }
             }
             else {
                 temp.loadTextures("res/inactive.png", "res/inactive.png", " ");
-
             }
         }
         else {
@@ -85,7 +86,7 @@ function createTable(s) {
         }
     }
 
-    function fullRefresh(x, y) {
+    function fullRefresh() {
         for (i = 1; i < 6; i++) {
             for (j = 1; j < 6; j++) {
                 refresh(i - 1, j - 1, map[(i - 1).toString() + " " + (j - 1).toString()][0]);
