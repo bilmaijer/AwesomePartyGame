a239efe946a5ba27e7f57cb259acc59a50750acf
Karl <karl.valliste@gmail.com>
2015-05-17 07:20:01 +0300

    liikumine paigas v2

--- src/methods.js
+++ src/methods.js
@@ -5,6 +5,11 @@ function createTable(s){
 
     var i;
     var j;
+    var array;
+    var kasutatud=[];
+    var asukoht = [2,-1];
+    var uus;
+    var test;
 
     //buttonite tegemise asi randomiga
 
@@ -19,41 +24,32 @@ function createTable(s){
 
     function onMouseUp(event, type) {
         if (type == ccui.Widget.TOUCH_ENDED){
-            var uus = event.name.split(" ");
-            var test = uus[0].toString() + " " + uus[1].toString();
-            var array = map[test];
+            uus = event.name.split(" ");
+            test = uus[0].toString() + " " + uus[1].toString();
+            array = map[test];
             s.addChild(hero, 250);
             var moveTo = new cc.MoveTo(2, cc.p(array[0].x, array[0].y));
             hero.runAction(moveTo);
-            //console.log("click");
+            if (asukoht[0] != null) {
+                kasutatud.push(asukoht[0].toString() + " " + asukoht[1].toString());
+                asukoht = (parseInt(uus[0]), parseInt(uus[1]));
+                refresh(parseInt(uus[0]), parseInt(uus[1]), array[0]);
+                //console.log("click");
+            }
         }
         //var array = map[event];
         //penCard(array.get(0), array.get(1))
     }
     //buttonite tegemise asi randomiga
 
-    var asukoht = [2,-1];
-
     for (i = 1; i < 6; i++) {
         for (j = 1; j < 6; j++) {
             var temp = new ccui.Button();
-            temp.x=cc.winSize.width / 6 * i;
-            temp.y=cc.winSize.height / 6 * j + cc.winSize.height / 12;
-            x=i-1;
-            y=j-1;
-            if (asukoht[0]-x===0||asukoht[0]-x===1||asukoht[0]-x===-1){
-                if (asukoht[1]-y===0||asukoht[1]-y===1||asukoht[1]-y===-1){
-                    temp.loadTextures("res/a5.png", "res/a4.png", " ");
-                    temp.addTouchEventListener(onMouseUp, this);
-                }
-                else{
-                    temp.loadTextures("res/b5.png", "res/b5.png", " ");
-                }
-            }
-            else{
-                temp.loadTextures("res/b5.png", "res/b5.png", " ");
-            }
-
+            temp.x = cc.winSize.width / 6 * i;
+            temp.y = cc.winSize.height / 6 * j + cc.winSize.height / 12;
+            x = i - 1;
+            y = j - 1;
+        refresh(x, y, temp);
 
             s.addChild(temp, 201);
             temp.name = (i-1).toString() + " " + (j-1).toString();
@@ -61,6 +57,25 @@ function createTable(s){
 
             var randInt = Math.floor(Math.random() * (content.length));
             map[temp.name] = [temp, content[randInt]];
+        }
+    }
+    function refresh(x, y, temp){
+        if (kasutatud.indexOf(x.toString()+" "+ y.toString())===-1) {
+            if (asukoht[0] - x === 0 || asukoht[0] - x === 1 || asukoht[0] - x === -1) {
+                if (asukoht[1] - y === 0 || asukoht[1] - y === 1 || asukoht[1] - y === -1) {
+                    temp.loadTextures("res/a5.png", "res/a4.png", " ");
+                    temp.addTouchEventListener(onMouseUp, this);
+                }
+                else {
+                    temp.loadTextures("res/b5.png", "res/b5.png", " ");
+                }
+            }
+            else {
+                temp.loadTextures("res/b5.png", "res/b5.png", " ");
+            }
+        }
+        else{
+            temp.loadTextures("res/c5.png", "res/c5.png", " ");
         }
     }
 }
