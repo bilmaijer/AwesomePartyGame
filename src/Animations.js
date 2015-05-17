b0e8137e911c85dcb53dea0eea9b1b412e24bae7
Anna <anna.bilmaijer@gmail.com>
2015-05-17 08:40:47 +0300

    loadtextures

--- src/Animations.js
+++ src/Animations.js
@@ -1,8 +1,50 @@
 /**
  * Created by Spikey on 16.05.2015.
  */
-function openCard(a, b){
-    console.log("click");
+function openCard(a, b, s){
+    console.log(a +" "+ b);
+
+    this.labelInstructions = new cc.LabelTTF("Drink!");
+    this.labelInstructions.setColor(cc.color(0,0,0));
+    var MySprite = new cc.Sprite("res/a4.png");
+
+    MySprite.attr({
+        x: a,
+        y: b,
+        scale: 1,
+        rotation: 0
+    });
+    //MySprite.flipY = true;
+
+
+    var next = new ccui.Button();
+
+
+
+
+    next.loadTextures("res/check.png", "res/check.png", " ");
+    next.setScale(0.5);
+    var labelX = MySprite.width;
+    var labelY = MySprite.height;
+
+
+    this.labelInstructions.setPosition(labelX/2, labelY/2);
+    next.setPosition(labelX/2, labelY/5)
+    MySprite.addChild(next, 252);
+    MySprite.addChild(this.labelInstructions, 252);
+    s.addChild(MySprite, 251);
+
+    var x = cc.winSize.width/2;
+    var y = cc.winSize.height/2;
+
+    //this.sprite
+    MySprite.runAction(
+        cc.spawn(
+            cc.scaleTo(1,4,4),
+            cc.moveTo(1,x,y)
+
+        )
+    );
 
 }
 function nextMove(a, b, hero) {
--- src/methods.js
+++ src/methods.js
@@ -1,13 +1,12 @@
 /**
  * Created by Spikey on 16.05.2015.
  */
-function createTable(s){
+function createTable(s) {
 
     var i;
     var j;
     var array;
-    var kasutatud=[];
-    var asukoht = [2,-1];
+    var kasutatud = [];
     var uus;
     var test;
 
@@ -23,11 +22,12 @@ function createTable(s){
     hero.y = cc.winSize.height / 12;
 
     function onMouseUp(event, type) {
-        if (type == ccui.Widget.TOUCH_ENDED){
-            uus = event.name.split(" ");
-            test = uus[0].toString() + " " + uus[1].toString();
-            array = map[test];
+        if (type == ccui.Widget.TOUCH_ENDED) {
+            var uus = event.name.split(" ");
+            var test = uus[0].toString() + " " + uus[1].toString();
+            var array = map[test];
             s.addChild(hero, 250);
+            openCard(array[0].x, array[0].y, s);
             var moveTo = new cc.MoveTo(2, cc.p(array[0].x, array[0].y));
             hero.runAction(moveTo);
             if (asukoht[0] != null) {
@@ -41,8 +41,12 @@ function createTable(s){
         //var array = map[event];
         //penCard(array.get(0), array.get(1))
     }
+
     //buttonite tegemise asi randomiga
 
+    var asukoht = [2, -1];
+
+    var empty = new ccui.Widget();
     for (i = 1; i < 6; i++) {
         for (j = 1; j < 6; j++) {
             var temp = new ccui.Button();
@@ -50,41 +54,43 @@ function createTable(s){
             temp.y = cc.winSize.height / 6 * j + cc.winSize.height / 12;
             x = i - 1;
             y = j - 1;
-        refresh(x, y, temp);
-
+            temp.loadTextures("res/active.png", "res/a4.png", " ", empty);
+            refresh(x, y, temp);
             s.addChild(temp, 201);
-            temp.name = (i-1).toString() + " " + (j-1).toString();
+            temp.name = (i - 1).toString() + " " + (j - 1).toString();
             //console.log(temp.name);
-
             var randInt = Math.floor(Math.random() * (content.length));
             map[temp.name] = [temp, content[randInt]];
         }
     }
-    function refresh(x, y, temp){
-        if (kasutatud.indexOf(x.toString()+" "+ y.toString())==-1) {
+    function refresh(x, y, temp) {
+        if (kasutatud.indexOf(x.toString() + " " + y.toString()) == -1) {
             if (asukoht[0] - x == 0 || asukoht[0] - x == 1 || asukoht[0] - x == -1) {
                 if (asukoht[1] - y == 0 || asukoht[1] - y == 1 || asukoht[1] - y == -1) {
                     temp.loadTextures("res/a5.png", "res/a4.png", " ");
                     temp.addTouchEventListener(onMouseUp, this);
                 }
                 else {
-                    temp.loadTextures("res/b5.png", "res/b5.png", " ");
+                    temp.loadTextures("res/inactive.png", "res/inactive.png", " ");
+
                 }
             }
             else {
-                temp.loadTextures("res/b5.png", "res/b5.png", " ");
+                temp.loadTextures("res/inactive.png", "res/inactive.png", " ");
+
             }
         }
-        else{
+        else {
             temp.loadTextures("res/c5.png", "res/c5.png", " ");
         }
     }
-    function fullRefresh(x, y){
+
+    function fullRefresh(x, y) {
         for (i = 1; i < 6; i++) {
             for (j = 1; j < 6; j++) {
-                refresh(i-1, j-1, map[(i-1).toString() + " " + (j-1).toString()][0]);
+                refresh(i - 1, j - 1, map[(i - 1).toString() + " " + (j - 1).toString()][0]);
             }
 
-            }
+        }
     }
 }
