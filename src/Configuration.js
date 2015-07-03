24f023f81da86dcbe0c4095d744cdc74532c92ed
Karl <karl.valliste@gmail.com>
2015-07-04 00:15:24 +0300

    Kõik läbi arutatud teemad sisse kantud, quit töötab, select 3 töötab

--- src/Configuration.js
+++ src/Configuration.js
@@ -62,7 +62,26 @@ var ConfigLayer = cc.Layer.extend({
                         finalThemes.push(buttons[r].key)
                     }
                 }
-                cc.director.runScene(new GameBoardScene())
+                if (finalThemes.length>2) {
+                    cc.director.runScene(new GameBoardScene())
+                }
+                else{
+                    var warning = new ccui.Text;
+                    warning.attr({
+                        string: "Minimum three themes!",
+                        fontName: "Marker Felt",
+                        fontSize: 80,
+                        anchorX: 0.6,
+                        anchorY: 0
+                    });
+                    warning.setTextHorizontalAlignment(cc.TEXT_ALIGNMENT_CENTER)
+                    warning.setColor(cc.color(255, 0, 0, 0));
+                    warning.setPositionX(230);
+                    warning.setPositionY(20);
+                    warning.setScale(0.25);
+                    warning.setFontName(_b_getFontName(res.robota));
+                    layout.addChild(warning, 252)
+                }
             }
         }
 
@@ -79,64 +98,6 @@ var ConfigLayer = cc.Layer.extend({
                 }
             }
         }
-
-        /*"""var sv = new ccui.ScrollView();
-         var widgetSize = cc.winSize;
-         sv.setContentSize(cc.size(widgetSize.width, widgetSize.height));
-         sv.anchorX = 0.5;
-         sv.anchorY = 0.5;
-         sv.x = widgetSize.width / 2;
-         sv.y = widgetSize.height / 2;
-         sv.setTouchEnabled(true);
-         sv.setDirection(ccui.ScrollView.DIR_VERTICAL);
-
-         var suvaline = new cc.LabelTTF();
-         //esimese korra funktsioon käivitatakse ainult siis, kui tuvastatakse, et local storage's puudub "keys"
-         //praegu on firstTime all alert, mis teatab, kui on esimene kord. Siis saame seda testida native appina.
-         //if (ls.getItem("keys") == null){
-         firstTime();
-         //}
-         var options = ls.getItem("keys").split(",");
-         var innerWidth = sv.getContentSize().width;
-         var innerHeight = sv.getContentSize().height + suvaline.getContentSize().height*options.length;
-         sv.setInnerContainerSize(cc.size(innerWidth, innerHeight));
-         suvaline.x = innerWidth / 2;
-         suvaline.y = innerHeight / 2;
-         sv.addChild(suvaline);
-         for (var i = 0; i < options.length; i++){
-         var tempBox = new ccui.CheckBox();
-         tempBox.setTouchEnabled(true);
-         tempBox.loadTextures("res/grey_circle.png","res/grey_boxTick.png","res/grey_boxTick.png","res/grey_circle.png","res/grey_boxTick.png");
-         tempBox.x = innerWidth / 2 + 30;
-         tempBox.y = innerHeight / options.length * i;
-         tempBox.string = options[i];
-         var temp = new cc.LabelTTF();
-         temp.setString(options[i]);
-         temp.x = innerWidth / 2;
-         temp.y = innerHeight / options.length * i;
-         temp.setEventListener(onMouseUp,this);
-         sv.addChild(temp);
-         }
-         this.addChild(sv);
-         */
-
-        //function firstTime() {
-        //    //TODO: OUR CONTENT GOES HERE
-        //    var keys = ["drinking", "sporty", "brainy", "tutvumis", "naughty"];
-        //    var drinking = ["Programmers drink!", "Pick three people to share their drinks with anyone who wishes", "Bottoms up!"];
-        //    var sporty = ["Jump off a cliff!", "Do jumping jacks!", "Do a barrel roll!"];
-        //    var brainy = ["Think reallllly hard", "Riddle the person to your right", "Ask the impossible question"];
-        //    var tutvumis = ["Ask the second person on your left three personal questions", "Everyone say their name", "please someone, let me out, i'm stuck in the card printing mach"];
-        //    var naughty = ["Lick a shoe or smth", "I don't know what you young people do.", "Back in my day..."];
-        //    //TODO: IF TESTING ON WINDOWS, UNCOMMENT. CHECKS IF IT DOES ACTUALLY SAVE.
-        //    //alert("Esimene kord!");
-        //    ls.setItem("keys", keys);
-        //    ls.setItem("drinking", drinking);
-        //    ls.setItem("sporty", sporty);
-        //    ls.setItem("brainy", brainy);
-        //    ls.setItem("tutvumis", tutvumis);
-        //    ls.setItem("naughty", naughty);
-        //}
         return true;
     }
 });
--- src/methods.js
+++ src/methods.js
@@ -199,11 +199,56 @@ function createTable(s) {
         //TODO: OUR CONTENT GOES HERE
         var map = {};
         var keys = ["drinking", "sporty", "brainy", "tutvumis", "naughty"];
-        var drinking = ["Programmers drink!", "Pick three people to share their drinks with anyone who wishes", "Bottoms up!"];
-        var sporty = ["Jump off a cliff!", "Do jumping jacks!", "Do a barrel roll!"];
-        var brainy = ["Think reallllly hard", "Riddle the person to your right", "Ask the impossible question"];
-        var tutvumis = ["Ask the second person on your left three personal questions", "Everyone say their name", "please someone, let me out, i'm stuck in the card printing mach"];
-        var naughty = ["Lick a shoe or smth", "I don't know what you young people do.", "Back in my day..."];
+        var drinking = [
+            "Make up a drinking rule. e.g. Who stands up for whatever reason, must drink",
+            "Pick three people to share their drinks with anyone who wishes",
+            "Window watchers - Windows phone users must drink",
+            "Partly mechanical - Android users must drink",
+            "Apple munchers drink - Everyone who has an iPhone must drink",
+            "Everyone (who's alive) drinks. Repeat as many times as you deem reasonable",
+            "Waterfall - On start everyone starts drinking and you may stop when the person to your left has stopped. Gamemaster will stop drinking first",
+            "Never have I ever - You say something that you have never done. Each person who has takes a sip of his drink. Repeat until each person has had his turn",
+            "We will order all of it - Gamemaster chooses one category and you start naming items that belongs in that category. Game over when someone can't think of anything new",
+            "Cavemen to the rescue - Every man must drink",
+            "Beauty pagent - Any woman is beautiful and there for they must drink"];
+        var sporty = [
+            "Ready, set, go! - First one to complete the following exercises, wins. 2x jumping jacks, sit up, push up, sit up, push up and a somersault",
+            "Repetition is the key - See who can do most push ups",
+            "Flamingo forest - Everyone stands on one leg with no support. Lets see who can last the longest",
+            "Follow the leader - The Gamemaster shows one physical exercise that the others must do. Then the others do the same in a clockwise order until everyone has had a go",
+            "",
+            "",
+            "",
+            ""];
+
+        var brainy = [
+            "Do the wordchain - Starting from the Gamemaster, everyone says a word. Words have two conditions: each word starts with the letter last one ended with and words must make full sentences",
+            "Everyday Joker - Riddle the person to your right",
+            "Guess my name - Gamemaster chooses 3 people from your group. Chosen people pick any person or character. The others ask yes/no questions to figure out the characters name",
+            "FizzBuzz- People start incrementally counting numbers moving clockwise through the group. For every number divisible by 3, say \"Fizz\" and for every number divisible by 5 say \"Buzz\". If the are divisible by both, say \"FizzBuzz\" and turn the circle around",
+            "Knigts and knaves - Knights always tell the truth, and knaves always lie.You meet two inhabitants: Peggy and Zippy. Peggy tells you that 'of Zippy and I, exactly one is a knight'. Zippy tells you that only a knave would say that Peggy is a knave. Who is a knight and who is a knave?",
+            "Pass the food around - Go around the group, each one naming a food item with eggs, flour and milk/water in it. You may not repeat",
+            "Gamemaster thinks of a word. The person on his right starts by saying one to three consecutive letters from the alphabet that are not part of this word. The next goes on as quick as he can",
+            ""
+            ];
+        var tutvumis = [
+            "Two truths and a lie - One by one each party member reveals two interesting facts about himself and adds one lie in the mix. The others try to guess the lie",
+            "Hi how are you! - Everyone says their name. Then each person in turn tries to correctly place all names. If someone fails, start over. Game ends, when everyone has memorized all names",
+            "Shorties - The person with the shortest pants/skirt asks the person with the shortest hair a personal question",
+            "Guess who I am - Game master chooses a person from the game and the others have to guess who it is by asking questions from him. \"Who is it?\" in any form is not allowed",
+            "Best drink i ever had - Everyone tell us about your favorite cocktail!",
+            "Describe the suspect - Describe yourself with three sentences. Your name doesn't count",
+            "You seem interesting - Compliment two other people who you find interesting",
+            ""];
+        var naughty = [
+            "Strip and tease - Each person chooses whatclothing item the person across from him/her must remove in a seductive manner",
+            "Touch this - Choose on person that will caress you. Person on your right dictates where you will be caressed",
+            "Fantasy close to home - GameMaster chooses two people that have to reveal to everyone who from your party he/she would take home. They must describe shortly what you would do to them",
+            "Clothes bazaar - Each person chooses one person from his/her desired sex that he will change one item of clothing that he/she wishes. Both of them will wear that item until the end of the game. You deside how naughty you make the game",
+            "",
+            "",
+            "",
+            ""];
         //alert("Esimene kord!");
         map["keys"] = keys;
         map["drinking"] = drinking;
